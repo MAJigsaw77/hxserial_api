bcbd8b95fcb5a0afad7d2c611514b74c17c4def9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-12 15:38:24 +0300

    Create Main.hx

---
+++ samples/basic/Main.hx
@@ -0,0 +1,25 @@
+package;
+
+import hxserialapi.SerialConnectionAPI;
+import hxserialapi.SerialDeviceAPI;
+import hxserialapi.Types;
+
+class Main
+{
+	public static function main():Void
+	{
+		final devices:cpp.RawPointer<SerialDevice> = untyped __cpp__('nullptr');
+
+		var count:cpp.SizeT = 0;
+
+		if (SerialDeviceTools.getSerialDevices(cpp.RawPointer.addressOf(devices), cpp.RawPointer.addressOf(count)))
+		{
+			for (i in 0...count)
+				Sys.println('Path: ${devices[i].path}, vID: ${devices[i].vid}, pID: ${devices[i].pid}.');
+
+			freeSerialDevices(devices, count);
+		}
+		else
+			Sys.println('No devices found.');
+	}
+}
