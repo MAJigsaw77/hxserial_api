e62d8c69cea5f76aafc3c3e60fca1bacbf3e7df8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-12 17:58:24 +0300

    Update serial_device.h

--- project/include/serial_device.h
+++ project/include/serial_device.h
@@ -11,19 +11,5 @@ typedef struct SerialDevice
 	int vID;
 } SerialDevice;
 
-/**
- * Get the list of connected serial devices.
- *
- * @param devices Output parameter that will point to an array of SerialDevice structures.
- * @param count Output parameter for the number of devices found.
- * @return true on success, false on failure.
- */
 bool get_serial_devices(SerialDevice **devices, size_t *count);
-
-/**
- * Free the memory allocated for the device list.
- *
- * @param devices The array of SerialDevice structures to be freed.
- * @param count The number of devices in the array.
- */
 void free_serial_devices(SerialDevice *devices, size_t count);
