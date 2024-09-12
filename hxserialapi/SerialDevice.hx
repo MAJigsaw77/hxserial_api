dc1a5379fd1f6b255440099003724feac1c1c7a5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-12 15:03:28 +0300

    Create SerialDevice.hx

---
+++ hxserialapi/SerialDevice.hx
@@ -0,0 +1,17 @@
+package hxserialapi;
+
+#if !cpp
+#error 'Serial API supports only C++ target platforms.'
+#end
+
+@:buildXml('<include name="${haxelib:hxserialapi}/project/Build.xml" />')
+@:include('serial_device.h')
+@:unreflective
+extern class SerialDevice
+{
+	@:native('get_serial_devices')
+	static function getSerialDevices(devices:cpp.RawPointer<cpp.RawPointer<SerialDevice>>, count:cpp.RawPointer<cpp.SizeT>):Bool;
+
+	@:native('free_serial_devices')
+	static function freeSerialDevices(devices:cpp.RawPointer<SerialDevice>, count:cpp.SizeT):Void;
+}
