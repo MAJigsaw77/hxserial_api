cd01481a185f4704796b0fb666fcb79bb9a11534
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-12 22:02:36 +0300

    Update serial_connection.hpp

--- project/include/serial_connection.hpp
+++ project/include/serial_connection.hpp
@@ -38,13 +38,13 @@ bool set_serial_connection_stop_bits(SerialConnection *connection, int stop_bits
 bool set_serial_connection_flow_control(SerialConnection *connection, int flow_control);
 bool set_serial_connection_timeout(SerialConnection *connection, int timeout);
 
-int read_serial_connection(const SerialConnection *connection, uint8_t *data, const size_t size);
-int read_byte_serial_connection(const SerialConnection *connection, uint8_t *data);
-int read_until_serial_connection(const SerialConnection *connection, uint8_t *data, const char until);
-int read_until_line_serial_connection(const SerialConnection *connection, uint8_t *data);
+int read_serial_connection(SerialConnection *connection, uint8_t *data, size_t size);
+int read_byte_serial_connection(SerialConnection *connection, uint8_t *data);
+int read_until_serial_connection(SerialConnection *connection, uint8_t *data, const char *until);
+int read_until_line_serial_connection(SerialConnection *connection, uint8_t *data);
 
-int has_available_data_serial_connection(const SerialConnection *connection);
+int has_available_data_serial_connection(SerialConnection *connection);
 
-int write_bytes_serial_connection(SerialConnection *connection, const uint8_t *data, const size_t size);
-int write_byte_serial_connection(SerialConnection *connection, const uint8_t data);
+int write_bytes_serial_connection(SerialConnection *connection, const uint8_t *data, size_t size);
+int write_byte_serial_connection(SerialConnection *connection, uint8_t data);
 int write_string_serial_connection(SerialConnection *connection, const char *data);
