16866ad7e6a3d157fab16622734bd509e0aaa7dd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-13 23:07:38 +0300

    Update Device.hx

--- hxserial_api/Device.hx
+++ hxserial_api/Device.hx
@@ -6,6 +6,27 @@ import hxserial_api.externs.Types;
 @:allow(hxserial_api.Serial)
 class Device
 {
+	public static function getDevices():Array<Device>
+	{
+		final devices:cpp.RawPointer<SerialDevice> = untyped __cpp__('nullptr');
+
+		var count:cpp.SizeT = 0;
+
+		if (SerialDeviceAPI.get_serial_devices(cpp.RawPointer.addressOf(devices), cpp.RawPointer.addressOf(count)))
+		{
+			final devicesList:Array<Device> = [];
+
+			for (i in 0...count)
+				devicesList.push(new Device(devices[i]));
+
+			SerialDeviceAPI.free_serial_devices(devices, count);
+
+			return devicesList;
+		}
+
+		return [];
+	}
+
 	public var path(get, never):String;
 	public var vID(get, never):Int;
 	public var pID(get, never):Int;
