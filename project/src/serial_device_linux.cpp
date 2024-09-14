9bf9632a0bbd0d189fb0d6513841365b0a391970
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-14 21:44:45 +0300

    Update serial_device_linux.cpp

--- project/src/serial_device_linux.cpp
+++ project/src/serial_device_linux.cpp
@@ -9,51 +9,59 @@
 
 bool get_serial_devices(SerialDevice **devices, size_t *count)
 {
-	struct udev *udev;
-	struct udev_enumerate *enumerate;
 	struct udev_list_entry *devices_list, *entry;
 	struct udev_device *dev;
 	struct udev_device *usb_dev;
+
 	SerialDevice *deviceList = NULL;
+
 	size_t deviceCount = 0;
 
-	udev = udev_new();
+	struct udev *udev = udev_new();
 
 	if (!udev)
 		return false;
 
-	enumerate = udev_enumerate_new(udev);
+	struct udev_enumerate *enumerate = udev_enumerate_new(udev);
 	udev_enumerate_add_match_subsystem(enumerate, "tty");
 	udev_enumerate_scan_devices(enumerate);
+
 	devices_list = udev_enumerate_get_list_entry(enumerate);
 
 	udev_list_entry_foreach(entry, devices_list)
 	{
-		const char *path;
-		path = udev_list_entry_get_name(entry);
-		dev = udev_device_new_from_syspath(udev, path);
+		const char *path = udev_list_entry_get_name(entry);
 
-		const char *dev_node = udev_device_get_devnode(dev);
-		usb_dev = udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
-
-		if (usb_dev)
+		if (path && path[0])
 		{
-			const char *vendor_id = udev_device_get_sysattr_value(usb_dev, "idVendor");
-			const char *product_id = udev_device_get_sysattr_value(usb_dev, "idProduct");
+			dev = udev_device_new_from_syspath(udev, path);
 
-			if (vendor_id && product_id)
-			{
-				deviceList = (SerialDevice *)realloc(deviceList, sizeof(SerialDevice) * (deviceCount + 1));
+			deviceList = (SerialDevice *)realloc(deviceList, sizeof(SerialDevice) * (deviceCount + 1));
 
+			const char *dev_node = udev_device_get_devnode(dev);
+
+			if (dev_node && dev_node[0])
 				deviceList[deviceCount].path = strdup(dev_node);
-				deviceList[deviceCount].vID = (int)strtol(vendor_id, NULL, 16);
-				deviceList[deviceCount].pID = (int)strtol(product_id, NULL, 16);
+
+			usb_dev = udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
+
+			if (usb_dev)
+			{
+				const char *vendor_id = udev_device_get_sysattr_value(usb_dev, "idVendor");
+
+				if (vendor_id && vendor_id[0])
+					deviceList[deviceCount].vID = (int)strtol(vendor_id, NULL, 16);
+
+				const char *product_id = udev_device_get_sysattr_value(usb_dev, "idProduct");
+
+				if (product_id && product_id[0])
+					deviceList[deviceCount].pID = (int)strtol(product_id, NULL, 16);
 
 				deviceCount++;
 			}
-		}
 
-		udev_device_unref(dev);
+			udev_device_unref(dev);
+		}
 	}
 
 	udev_enumerate_unref(enumerate);
