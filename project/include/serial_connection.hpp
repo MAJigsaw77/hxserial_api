b22d0ac283786aae67c56b46c47b7707532aa230
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-15 17:31:11 +0300

    Update serial_connection.hpp

--- project/include/serial_connection.hpp
+++ project/include/serial_connection.hpp
@@ -44,13 +44,5 @@ bool set_serial_connection_flow_control(SerialConnection *connection, int flow_c
 bool set_serial_connection_timeout(SerialConnection *connection, int timeout);
 
 int read_serial_connection(SerialConnection *connection, uint8_t *data, size_t size);
-int read_byte_serial_connection(SerialConnection *connection, uint8_t *data);
-int read_until_byte_serial_connection(SerialConnection *connection, uint8_t *data, uint8_t until);
-int read_until_string_serial_connection(SerialConnection *connection, uint8_t *data, const char *until);
-int read_until_line_serial_connection(SerialConnection *connection, uint8_t *data);
-
 int has_available_data_serial_connection(SerialConnection *connection);
-
 int write_bytes_serial_connection(SerialConnection *connection, const uint8_t *data, size_t size);
-int write_byte_serial_connection(SerialConnection *connection, uint8_t data);
-int write_string_serial_connection(SerialConnection *connection, const char *data);
