1243007f5c145f68e4501b98c626a5789a68ed9d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-15 10:45:23 +0300

    Update Device.hx

--- hxserial_api/Device.hx
+++ hxserial_api/Device.hx
@@ -3,9 +3,18 @@ package hxserial_api;
 import hxserial_api.externs.SerialDeviceAPI;
 import hxserial_api.externs.Types;
 
+/**
+ * Represents a serial device and provides methods to retrieve available devices
+ * and device-specific information such as path, vendor ID (vID), and product ID (pID).
+ */
 @:allow(hxserial_api.Connection)
 class Device
 {
+	/**
+	 * Retrieves a list of available serial devices connected to the system.
+	 *
+	 * @return An array of `Device` objects representing the available serial devices.
+	 */
 	public static function getDevices():Array<Device>
 	{
 		final devices:cpp.RawPointer<SerialDevice> = untyped __cpp__('nullptr');
@@ -24,13 +33,32 @@ class Device
 		return [];
 	}
 
+	/**
+	 * The file path of the serial device.
+	 */
 	public var path(get, never):String;
+
+	/**
+	 * The vendor ID (vID) of the serial device.
+	 */
 	public var vID(get, never):Int;
+
+	/**
+	 * The product ID (pID) of the serial device.
+	 */
 	public var pID(get, never):Int;
 
+	/**
+	 * Internal structure representing the serial device.
+	 */
 	@:noCompletion
 	private var device:cpp.Struct<SerialDevice>;
 
+	/**
+	 * Constructor for creating a new `Device` instance based on a `SerialDevice` structure.
+	 *
+	 * @param device The `SerialDevice` structure representing the serial device.
+	 */
 	public function new(device:cpp.Struct<SerialDevice>):Void
 	{
 		this.device = device;
